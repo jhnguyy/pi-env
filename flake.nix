215438b9b7d447656e08d30b6ed8493571189108
Jonathan Nguyen <jonathan.nguyen@plenful.com>
2026-09-25 18:32:52 -0700

    chore(toolchain): upgrade Nub to 0.9.5

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nub = {
-      url = "github:nubjs/nub/v0.9.2";
+      url = "github:nubjs/nub/v0.9.5";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
